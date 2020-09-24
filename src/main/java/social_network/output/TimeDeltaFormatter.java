e590e0d7811b3b14a8c03b49b6976bbb9397514d
ewansheldon <ewansheldon@gmail.com>
2020-09-24 14:59:25 +0100

    calculates time delta in minutes

--- src/main/java/social_network/output/TimeDeltaFormatter.java
+++ src/main/java/social_network/output/TimeDeltaFormatter.java
@@ -4,8 +4,14 @@ import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
 public class TimeDeltaFormatter {
+
+    private static final int MINUTE = 60;
+
     public String format(LocalDateTime date) {
         long diff = ChronoUnit.SECONDS.between(date, LocalDateTime.now());
+        if (diff >= MINUTE) {
+            return String.format("%s minutes ago", diff / MINUTE);
+        }
         return String.format("%s seconds ago", diff);
     }
 }
--- src/test/java/social_network/output/TimeDeltaFormatterShould.java
+++ src/test/java/social_network/output/TimeDeltaFormatterShould.java
@@ -1,5 +1,6 @@
 package social_network.output;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDateTime;
@@ -8,11 +9,25 @@ import java.time.temporal.ChronoUnit;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TimeDeltaFormatterShould {
+
+    private TimeDeltaFormatter timeDeltaFormatter;
+
+    @BeforeEach
+    void setUp() {
+        timeDeltaFormatter = new TimeDeltaFormatter();
+    }
+
     @Test
     void formats_time_difference_of_less_than_minute_in_seconds() {
-        TimeDeltaFormatter timeDeltaFormatter = new TimeDeltaFormatter();
         LocalDateTime date = LocalDateTime.now().minus(2, ChronoUnit.SECONDS);
         String delta = timeDeltaFormatter.format(date);
         assertEquals("2 seconds ago", delta);
+    }
+
+    @Test
+    void formats_time_difference_of_less_than_an_hour_in_minutes() {
+        LocalDateTime date = LocalDateTime.now().minus(24, ChronoUnit.MINUTES);
+        String delta = timeDeltaFormatter.format(date);
+        assertEquals("24 minutes ago", delta);
     }
 }
